925123925bde96638e62ef36fb4d6f8de60884d8
Wil Erickson <william@ericksonls.com>
2023-11-23 20:38:18 -0800

    finished refactoring

--- exercise-8.2.rb
+++ exercise-8.2.rb
@@ -2,7 +2,11 @@ class Poker
   def initialize(players)
     @players = players
     @hands = []
-    players.length().times { |x| @hands.append(nil) }
+    initialize_players
+  end
+
+  def initialize_players
+    @players.length().times { |x| @hands.append(nil) }
   end
 
   def play_poker()
@@ -31,7 +35,7 @@ class Chess
 
   def play_game()
     puts "Players in the chess game:"
-    @players.length().times { |x| puts "#{self.get_player_name(x)}: #{@players[x][1]}" }
+    @players.each { |name, color| puts "#{name}: #{color}"}
     # [pretend there's code here]
   end
 
@@ -47,7 +51,7 @@ end
 class GoPlayer
   attr_reader :name, :color
 
-  def initialize(name, color)
+  def initialize(name:, color:)
     @name = name
     @color = color
   end
@@ -56,9 +60,18 @@ end
 class Go
   def initialize(players)
     @players = []
-    players.each { |x, y| @players.append(GoPlayer.new(x, y)) }
+    create_players(players)
   end
-
+  
+  def create_players(players)
+    players.each { |name, color| @players.append(create_go_player(name, color)) }
+  end
+  
+  # wrap outside class
+  def create_go_player(name, color)
+    GoPlayer.new(name: name, color: color)
+  end
+  
   def play()
     puts "Players in the go game:"
     @players.each { |player| puts "#{player.name}: #{player.color}" }
@@ -70,41 +83,56 @@ class Go
   end
 end
 
+# We really don't need this whole class anyways, but it seems like a fair way to handle events for this situation, separate game functions would be best
+# though this class both has lots of dependencies, and uses flags, wrapping the functions can help this function be cleaner
 class PlayGames
 
-  def initialize(game_number, player_list)
+  def initialize(game_name:, player_list:)
     @player_list = player_list
-    @game_number = game_number
+    @game_name = game_name
+  end
+  
+  def play_poker()
+    poker = Poker.new(@player_list)
+    poker.play_poker()
+    puts poker.get_results()
   end
 
+  def play_chess()
+    chess = Chess.new(@player_list)
+    chess.play_game()
+    puts chess.get_results()
+  end
+  
+  def play_go()
+    go = Go.new(@player_list)
+    go.play()
+    puts go.get_score()
+  end
+  
   def play()
-    case @game_number
-    when 1
-      poker = Poker.new(@player_list)
-      poker.play_poker()
-      puts poker.get_results()
-    when 2
-      chess = Chess.new(@player_list)
-      chess.play_game()
-      puts chess.get_results()
-    when 3
-      go = Go.new(@player_list)
-      go.play()
-      puts go.get_score()
+    case @game_name
+    when "poker"
+      play_poker
+    when "chess"
+      play_chess
+    when "go"
+      play_go
     end
   end
+  
 end
 
-pg = PlayGames.new(1, ["alice", "bob", "chris", "dave"])
+pg = PlayGames.new(game_name: "poker", player_list: ["alice", "bob", "chris", "dave"])
 pg.play()
 
 puts
 
-pg = PlayGames.new(2, [["alice", "white"], ["bob", "black"]])
+pg = PlayGames.new(game_name: "chess", player_list: [["alice", "white"], ["bob", "black"]])
 pg.play()
 
 puts
 
-pg = PlayGames.new(3, [["alice", "white"], ["bob", "black"]])
+pg = PlayGames.new(game_name: "go", player_list: [["alice", "white"], ["bob", "black"]])
 pg.play()
 
